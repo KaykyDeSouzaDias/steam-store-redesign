303fb39f74228595fe3e3418aff4b1e8d62bcede
Kayky Dias <kayky.dias@brilliantmachine.com.br>
2022-10-20 16:07:12 -0300

    - header menu refactoring & responsivity

---
+++ js/header.js
@@ -0,0 +1,6 @@
+function openMenuSideNav() {
+  document.getElementById("menuSidenav").style.width = "250px";
+}
+function closeMenuSideNav() {
+  document.getElementById("menuSidenav").style.width = "0";
+}
