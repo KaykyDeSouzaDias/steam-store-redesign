1d99ad47812ed26a0f352d4b0506a847e4e8cabf
Kayky Dias <kayky.dias@brilliantmachine.com.br>
2022-10-25 17:35:54 -0300

    - hot deals refactoring & responsivity

--- js/hot-deals.js
+++ js/hot-deals.js
@@ -9,49 +9,44 @@ export function displayValueGamesAndHotDeals() {
       return `
         <div id="hot-deals-${deal.id}" class="deal-games">
             <div class="deal-games-detail">
+              ${
+                deal.review === ""
+                  ? `
+                  <div class="deal-type">
+                      <i class="${deal.gameCategoryTypeIcon}"></i>
+                      <p>${deal.gameCategoryType}</p>
+                  </div>
+                  `
+                  : `
+                  <div class="deal-type">
+                      <i class="${deal.reviewIcon}" style="color: ${deal.reviewIconColor}"></i>
+                      <p>${deal.review}</p>
+                  </div>
+                  `
+              }
   
-                ${
-                  deal.review === ""
-                    ? `
-                    <div class="category-type">
-                        <span class="material-symbols-outlined">
-                            ${deal.gameCategoryTypeIcon}
-                        </span>
-                        <p class="top-seller-txt">${deal.gameCategoryType}</p>
-                    </div>
-                    `
-                    : `
-                    <div class="category-type">
-                        <span class="material-symbols-outlined">
-                            ${deal.reviewIcon}
-                        </span>
-                        <p class="top-seller-txt">${deal.review}</p>
-                    </div>
-                    `
-                }
-    
-                ${
-                  deal.isInOffer
-                    ? `
-                    <div class="deal-sale-price">
+              ${
+                deal.isInOffer
+                  ? `
+                  <div class="deal-sale-price">
                     <h3>${deal.dealOffer}%</h3>
-                    </div>
+                  </div>
 
-                    <div class="deal-games-price">
+                  <div class="deal-games-price">
                     <h3 class="deal-old-price">R$${deal.old_price}</h3>
                     <h3 class="deal-new-price">R$${deal.actual_price}</h3>
-                    </div>
-                `
-                    : `
-                    <div class="deal-games-price">
-                        <h3 class="deal-normal-price">${
-                          deal.actual_price === "Free"
-                            ? `${deal.actual_price}`
-                            : `R$${deal.actual_price}`
-                        }</h3>
-                    </div>
-                    `
-                }
+                  </div>
+              `
+                  : `
+                  <div class="deal-games-price">
+                    <h3 class="deal-normal-price">${
+                      deal.actual_price === "Free"
+                        ? `${deal.actual_price}`
+                        : `R$${deal.actual_price}`
+                    }</h3>
+                  </div>
+                  `
+              }
   
             </div>
         </div>
