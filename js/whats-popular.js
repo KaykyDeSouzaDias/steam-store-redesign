00d85fe27eea9949776f662897d7aa6b385693e1
Kayky Dias <kayky.dias@brilliantmachine.com.br>
2022-10-25 14:22:13 -0300

    - whats popular refactoring & responsivity

--- js/whats-popular.js
+++ js/whats-popular.js
@@ -13,47 +13,51 @@ export function displayWhatsPopularGames() {
       <div>
         <img class="popular-games-image" src="${popular.image}" alt="">
       </div>
-      <div>
-        <h1 class="popular-games-title">${popular.name}</h1>
-        <div class="popular-games-keywords">
-          ${popular.gameKeywords
-            .map((key, index) => {
-              if (index === 3) {
-                return `
-                <p id="slider-keyword-1">${key}</p>
-              `;
-              } else {
-                return `
-                <p id="slider-keyword-1">${key}</p>
-                <i class="fa-solid fa-circle"></i>
-              `;
-              }
-            })
-            .join("")}
+      <div class="popular-games-detail">
+        <div>
+          <h1 class="popular-games-title">${popular.name}</h1>
+          <div class="popular-games-keywords">
+            ${popular.gameKeywords
+              .map((key, index) => {
+                if (index === 3) {
+                  return `
+                  <p id="slider-keyword-1">${key}</p>
+                `;
+                } else {
+                  return `
+                  <p id="slider-keyword-1">${key}</p>
+                  <i class="ph-circle-fill"></i>
+                `;
+                }
+              })
+              .join("")}
+          </div>
         </div>
-      </div>
-      ${
-        popular.isInOffer
-          ? `
-        <div class="recommended-sale-price">
-          <h3>${popular.dealOffer}%</h3>
-        </div>
+        ${
+          popular.isInOffer
+            ? `
+            <div class="popular-games-offer">
+              <div class="popular-sale-price">
+                <h3>${popular.dealOffer}%</h3>
+              </div>
 
-        <div class="recommended-games-price">
-          <h3 class="recommended-old-price">R$${popular.old_price}</h3>
-          <h3 class="recommended-new-price">R$${popular.actual_price}</h3>
-        </div>
+              <div class="popular-games-offer-price">
+                <h3 class="popular-old-price">R$${popular.old_price}</h3>
+                <h3 class="popular-new-price">R$${popular.actual_price}</h3>
+              </div>
+            </div>
+          `
+            : `
+          <div class="popular-games-price">
+            <h3 class="popular-games-normal-price">${
+              popular.actual_price === "Free"
+                ? `${popular.actual_price}`
+                : `R$${popular.actual_price}`
+            }</h3>
+          </div>
         `
-          : `
-        <div class="popular-games-price">
-          <h3 class="popular-games-normal-price">${
-            popular.actual_price === "Free"
-              ? `${popular.actual_price}`
-              : `R$${popular.actual_price}`
-          }</h3>
-        </div>
-      `
-      }
+        }
+      </div>
     </li>
   `;
     })
